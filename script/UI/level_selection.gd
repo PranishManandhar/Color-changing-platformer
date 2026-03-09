f088dc341f04ec3c2ec86ac2903edcd524a16e46
PranishManandhar <mdrpranish00@gmail.com>
2026-03-09 12:03:13 +0545

    updated ui for start screen and a level selection and now game can be closed

--- script/UI/level_selection.gd
+++ script/UI/level_selection.gd
@@ -4,20 +4,22 @@ var levels = [
 	"res://scenes/prefabs/UI/main_menu.tscn",
 	"res://scenes/Levels/lvl1.tscn",
 	"res://scenes/Levels/lvl2.tscn",
+	"res://scenes/Levels/lvl_3.tscn"
 ]
 
 func _ready() -> void:
 	$"CanvasLayer/Back".pressed.connect(Callable(self,"_on_back_pressed"))
-	$"CanvasLayer/lvl 1".pressed.connect(Callable(self, "_on_lvl_1_pressed"))
-	$"CanvasLayer/lvl 2".pressed.connect(Callable(self,"_on_lvl_2_pressed"))
-
+	$"CanvasLayer/Panel/VBoxContainer/lvl 1".pressed.connect(Callable(self, "_on_lvl_1_pressed"))
+	$"CanvasLayer/Panel/VBoxContainer/lvl 2".pressed.connect(Callable(self,"_on_lvl_2_pressed"))
+	$"CanvasLayer/Panel/VBoxContainer/lvl 3".pressed.connect(Callable(self,"_on_lvl_3_pressed"))
 func _on_back_pressed() -> void:
 	call_deferred("_level_load", levels[0])
 func _on_lvl_1_pressed() -> void:
 	call_deferred("_level_load", levels[1])
 func _on_lvl_2_pressed() -> void:
 	call_deferred("_level_load", levels[2])
-
+func _on_lvl_3_pressed() -> void:
+	call_deferred("_level_load", levels[2])
 
 func _level_load(scene_path:String):
 	if ResourceLoader.exists(scene_path):
--- script/UI/main_menu.gd
+++ script/UI/main_menu.gd
@@ -1,12 +1,16 @@
 extends Control
 
 var new_page = preload("res://scenes/prefabs/UI/level_selection.tscn")
+@onready var bottom_label:Label = $CanvasLayer/bottomLabel/bottomLabel
 
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_packed(new_page)
 	
 func _on_settings_pressed() -> void:
-	print("Sorry This function is not available for now")
+	bottom_label.text = "Sorry This function is not available for now"
 
 func _on_about_pressed() -> void:
-	pass # Replace with function body.
+	bottom_label.text = "This game is produced by Gaminglabplays"
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
